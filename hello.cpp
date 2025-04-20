5660393bf4341207f884c02db7cdff2a85dc70f8
shor-ofer <shor.ofer@live.com>
2025-04-20 09:53:09 +0300

    Update hello.cpp
    123

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World from CMake - first change" << std::endl;
+    std::cout << "Hello, World from CMake - first change123" << std::endl;
     return 0;
 }
